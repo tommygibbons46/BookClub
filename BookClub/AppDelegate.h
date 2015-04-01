638bcede93ec9e68480b34abcbfb7c1712622e2f
Tommy Gibbons <tommygibbons@Tommys-MacBook-Pro-2.local>
2015-04-01 14:14:53 -0700

    Initial Commit

---
+++ BookClub/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  BookClub
+//
+//  Created by Tommy Gibbons on 4/1/15.
+//  Copyright (c) 2015 Tommy Gibbons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
